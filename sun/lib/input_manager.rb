820d39c495a60ff1ded5272801e4966f08241398
bg <b.goodspeed@gmail.com>
2012-03-29 15:59:03 -0300

    input manager refactoring

--- sun/lib/input_manager.rb
+++ sun/lib/input_manager.rb
@@ -1,96 +1,92 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
+module KeyActions
+  UP = "Up"
+  RIGHT = "Right"
+  LEFT = "Left"
+  DOWN = "Down"
+  FIRE = "Fire"
+  QUIT = "Quit"
+  MENU = "Menu"
+  MENU_ENTER = "MenuEnter"
+end
+
 class InputManager
-  def initialize(game)
+  attr_accessor :keyboard, :gamepad
+  def self.default_keyboard_config
+    { Gosu::KbLeft => KeyActions::LEFT,
+      Gosu::KbRight => KeyActions::RIGHT,
+      Gosu::KbUp => KeyActions::UP,
+      Gosu::KbDown => KeyActions::DOWN,
+      Gosu::KbSpace => KeyActions::FIRE,
+      Gosu::KbEnter => KeyActions::MENU_ENTER,
+      Gosu::KbReturn => KeyActions::MENU_ENTER,
+      Gosu::KbM => KeyActions::MENU,
+      Gosu::KbQ => KeyActions::QUIT }
+
+  end
+  def self.default_gamepad_config
+    { Gosu::GpLeft => KeyActions::LEFT,
+      Gosu::GpRight => KeyActions::RIGHT,
+      Gosu::GpUp => KeyActions::UP,
+      Gosu::GpDown => KeyActions::DOWN }
+  end
+
+  #TODO abstract these into yml ?
+  def initialize(game, keyboard_conf = InputManager.default_keyboard_config, gamepad_conf = InputManager.default_gamepad_config)
     @game = game
     @keys = {}
+    @keyboard = keyboard_conf
+    @gamepad = gamepad_conf
+
+    #TODO how to make this configurable? maybe not needed since input->action is configurable
+    @always_available_behaviors = {
+      KeyActions::QUIT => lambda { @game.deactivate_and_quit },
+    }
+    @gameplay_behaviors = {
+      KeyActions::MENU => lambda { @game.enter_menu },
+      KeyActions::RIGHT => lambda { @game.player.turn(@game.turn_speed) },
+      KeyActions::LEFT => lambda { @game.player.turn(-@game.turn_speed) },
+      KeyActions::UP => lambda { @game.player.move_forward(@game.movement_distance) },
+      KeyActions::DOWN => lambda { @game.player.move_forward(-@game.movement_distance) },
+      KeyActions::FIRE => lambda { @game.player.use_weapon },
+    }
+
+    @menu_behaviors = {
+      KeyActions::DOWN => lambda { @game.menu_manager.move_down},
+      KeyActions::MENU_ENTER => lambda { @game.menu_manager.invoke_current}
+    }
+
   end
-  @@UP = "Up"
-  @@RIGHT = "Right"
-  @@LEFT = "Left"
-  @@DOWN = "Down"
-  @@FIRE = "Fire"
-  @@QUIT = "Quit"
-  @@MENU = "Menu"
-  @@MENU_ENTER = "MenuEnter"
+
+  def run_activated(behaviors)
+    behaviors.each { |action, behavior| behavior.call if @keys[action] }
+  end
 
   def respond_to_keys
-    if @keys[@@QUIT]
-      @game.deactivate_and_quit
-    end
 
-    if @keys[@@MENU]
-      @game.enter_menu
-    end
-
+    run_activated @always_available_behaviors
     
     if @game.menu_mode?
-      if @keys[@@DOWN]
-        @game.menu_manager.move_down
-      end
-      if @keys[@@MENU_ENTER]
-        @game.menu_manager.invoke_current
-      end
-
+      run_activated @menu_behaviors
       @game.update_menu_state
       return
     end
 
-    if @keys[@@RIGHT]
-      @game.player.turn(@game.turn_speed)
-    end
-    if @keys[@@LEFT]
-      @game.player.turn(-@game.turn_speed)
-    end
-    if @keys[@@UP]
-      @game.player.move_forward(@game.movement_distance)
-    end
-    if @keys[@@DOWN]
-      @game.player.move_forward(-@game.movement_distance)
-    end
-    if @keys[@@FIRE]
-      @game.player.use_weapon
-    end
+    run_activated @gameplay_behaviors
 
   end
 
+
+  def button_mappings
+    @gamepad.merge(@keyboard)
+  end
+
   def update_key_state
-
-    if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft then
-      set_key_to_active(@@LEFT)
+    button_mappings.each do |btn, mapping|
+      set_key_to_active(mapping) if button_down? btn
     end
-    if button_down? Gosu::KbRight or button_down? Gosu::GpRight then
-      set_key_to_active(@@RIGHT)
-    end
-    if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
-      set_key_to_active(@@UP)
-    end
-    if button_down? Gosu::KbDown or button_down? Gosu::GpButton1 then
-      set_key_to_active(@@DOWN)
-    end
-    if button_down? Gosu::KbSpace then
-      set_key_to_active(@@FIRE)
-    end
-    if button_down? Gosu::KbEnter or button_down? Gosu::KbReturn then
-      set_key_to_active(@@MENU_ENTER)
-    end
-
-    if button_down? Gosu::KbQ then
-      set_key_to_active(@@QUIT)
-    end
-
-    if button_down? Gosu::KbM then
-      set_key_to_active(@@MENU)
-    end
-
-    #if button_down? Gosu::KbA then
-    #  toggle_enemy_ai
-    #end
-    if button_down? Gosu::KbA then
-      toggle_enemy_ai
-    end
-
   end
 
   def button_down?(button)
