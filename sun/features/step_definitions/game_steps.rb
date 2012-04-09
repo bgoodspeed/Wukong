42b7488840eddd30fc6aab65ce48bc7048b9ea57
bg <b.goodspeed@gmail.com>
2012-04-09 13:06:03 -0300

    HACK HACK HACK

--- sun/features/step_definitions/game_steps.rb
+++ sun/features/step_definitions/game_steps.rb
@@ -1,10 +1,51 @@
 
+def stub_window
+  m = Mocha::Mock.new("gamewindow")
+  m.stubs(:window).returns m
+  m.stubs(:button_down?).returns true #HACK
+  m
+end
+
+def stub_song
+  m = Mocha::Mock.new("song")
+  m.stubs(:play).returns
+  m
+end
+def stub_sample
+  m = Mocha::Mock.new("sample")
+  m.stubs(:play).returns
+  m
+end
+
+def stub_font
+  m = Mocha::Mock.new("font")
+  m
+end
+def stub_image
+  m = Mocha::Mock.new("image")
+  m.stubs(:width).returns 36
+  m.stubs(:height).returns 36 #HACK
+  m
+end
+
+def stub_out_gosu
+  GameWindow.stubs(:new).returns(stub_window)
+  Gosu::Font.stubs(:new).returns(stub_font)
+  Gosu::Image.stubs(:new).returns(stub_image)
+  Gosu::Image.stubs(:load_tiles).returns(stub_image)
+  Gosu::Sample.stubs(:new).returns(stub_sample)
+  Gosu::Song.stubs(:new).returns(stub_song)
+end
+
 
 Given /^I load the game on level "([^"]*)" with screen size (\d+), (\d+)$/ do |level_to_load, width, height|
+  stub_out_gosu
+
   @game = Game.new({:width => width.to_i, :height => height.to_i})
   @level = @game.load_level("test-data/levels/#{level_to_load}/#{level_to_load}.yml")
 end
 Given /^I load the game "([^"]*)"$/ do |arg1|
+  stub_out_gosu
   @game = YamlLoader.game_from_file("test-data/#{arg1}.yml")
 end
 
