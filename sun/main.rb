c3168585efa8b303005fcb3b4a4e1ab0daeba576
bg <b.goodspeed@gmail.com>
2012-04-17 10:02:13 -0300

    updated main so it can use cmd line args

--- sun/main.rb
+++ sun/main.rb
@@ -5,8 +5,11 @@ $: << "lib"
 
 require "game"
 
-
-game = YamlLoader.game_from_file("game-data/new_game_load_screen.yml")
+if ARGV.empty?
+  game = YamlLoader.game_from_file("game-data/new_game_load_screen.yml")
+else
+  game = YamlLoader.game_from_file(ARGV.first)
+end
 
 
 #TODO this should be triggered by and stored in the level
