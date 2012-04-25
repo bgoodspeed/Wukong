ab246ab496909befe9c1d813fc16ea5463f066d2
bg <b.goodspeed@gmail.com>
2012-04-25 12:05:55 -0300

    added full fledged view classes instead of lambdas

--- sun/lib/controllers/rendering_controller.rb
+++ sun/lib/controllers/rendering_controller.rb
@@ -1,59 +1,96 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
+module Views
+  class BaseView
+    include UtilityDrawing
+    attr_reader :game
+    def initialize(game)
+      @game = game
+    end
+  end
+  class LineSegmentView < BaseView
+    def call(screen, linesegment)
+      p1 = game.camera.screen_coordinates_for(linesegment.p1)
+      p2 = game.camera.screen_coordinates_for(linesegment.p2)
+      ls = Primitives::LineSegment.new(p1, p2)
+      draw_line_segment(screen, ls, ZOrder.static.value, Graphics::Color::RED)
+    end
+  end
+
+  class PlayerView < BaseView
+    def call(screen, player)
+      game.image_controller.draw_in_screen_coords(player)
+    end
+  end
+
+  class EnemyView < BaseView
+    def call(screen, enemy)
+      game.image_controller.draw_in_screen_coords(enemy)
+    end
+  end
+  class EventAreaView < BaseView
+    def call(screen, ea)
+      r = ea.rect
+      rect = Primitives::Rectangle.new(game.camera.screen_coordinates_for(r.p1), game.camera.screen_coordinates_for(r.p2), game.camera.screen_coordinates_for(r.p3), game.camera.screen_coordinates_for(r.p4))
+      #TODO this is getting messy
+      msg_offset = [15,15]
+      game.font_controller.draw_with_font(ea.label, rect.p1.x + msg_offset.x, rect.p1.y + msg_offset.y, ZOrder.hud.value)
+      draw_rectangle_as_box(screen, rect,  ZOrder.static.value, color=Graphics::Color::BLACK)
+    end
+  end
+
+  class InfoWindowView < BaseView
+    def call(screen, iw)
+      darken_screen(game, 50, game.window.width-50, 50, game.window.height-50)
+      #TODO fix this ZOrder stupidity
+      iw.description.each_with_index do |desc, idx|
+        game.font_controller.draw_with_font(desc, 60, 60*(idx + 1), ZOrder.hud.value)
+      end
+    end
+  end
+  class WeaponView < BaseView
+    def call(screen, w)
+      lsi = w.to_collision
+
+      ls1 = game.camera.screen_coordinates_for(lsi.p1)
+      ls2 = game.camera.screen_coordinates_for(lsi.p2)
+      ls = Primitives::LineSegment.new(ls1,ls2)
+      draw_line_segment(screen, ls)
+    end
+  end
+  class VectorFollowerView < BaseView
+    def call(screen, vf)
+      d = 10
+      cp = game.camera.screen_coordinates_for(vf.current_position)
+
+      draw_rectangle(screen, Primitives::Rectangle.new(cp, cp.plus([d,0]), cp.plus([d,d]), cp.plus([0,d])))
+      lsi = vf.to_collision
+
+      ls1 = game.camera.screen_coordinates_for(lsi.p1)
+      ls2 = game.camera.screen_coordinates_for(lsi.p2)
+      ls = Primitives::LineSegment.new(ls1,ls2)
+      draw_line_segment(screen, ls)
+    end
+  end
+
+
+end
+
+
 class RenderingController
 
   def initialize(game)
     @game = game
-    @mapping = {Primitives::LineSegment => lambda {|screen, linesegment|
-                 p1 = @game.camera.screen_coordinates_for(linesegment.p1)
-                 p2 = @game.camera.screen_coordinates_for(linesegment.p2)
-                 ls = Primitives::LineSegment.new(p1, p2)
-                 draw_line_segment(screen, ls, ZOrder.static.value, Graphics::Color::RED)
-               },
-               Player => lambda {|screen, player| @game.image_controller.draw_in_screen_coords(player) },
-               Enemy => lambda {|screen, enemy| @game.image_controller.draw_in_screen_coords(enemy) },
-               EventArea => lambda {|screen, ea|
-                 r = ea.rect
-                 rect = Primitives::Rectangle.new(@game.camera.screen_coordinates_for(r.p1), @game.camera.screen_coordinates_for(r.p2), @game.camera.screen_coordinates_for(r.p3), @game.camera.screen_coordinates_for(r.p4))
-                 #TODO this is getting messy
-                 msg_offset = [15,15]
-                 @game.font_controller.draw_with_font(ea.label, rect.p1.x + msg_offset.x, rect.p1.y + msg_offset.y, ZOrder.hud.value)
-                 draw_rectangle_as_box(screen, rect,  ZOrder.static.value, color=Graphics::Color::BLACK)
-#                 puts "draw event area at #{ea.rect}, with text #{ea.label} inside"
-               },
-               InfoWindow => lambda {|screen, iw|
-
-                 darken_screen(@game, 50, @game.window.width-50, 50, @game.window.height-50)
-                 #TODO fix this ZOrder stupidity
-                 iw.description.each_with_index do |desc, idx|
-                  @game.font_controller.draw_with_font(desc, 60, 60*(idx + 1), ZOrder.hud.value)
-                 end
-                 
-               },
-               Weapon => lambda {|screen, w|
-                 lsi = w.to_collision
-
-                 ls1 = @game.camera.screen_coordinates_for(lsi.p1)
-                 ls2 = @game.camera.screen_coordinates_for(lsi.p2)
-                 ls = Primitives::LineSegment.new(ls1,ls2)
-                 draw_line_segment(screen, ls)
-
-               },
+    @mapping = {Primitives::LineSegment => Views::LineSegmentView.new(@game),
+               Player => Views::PlayerView.new(@game),
+               Enemy => Views::EnemyView.new(@game),
+               EventArea => Views::EventAreaView.new(@game),
+               InfoWindow => Views::InfoWindowView.new(@game),
+               Weapon => Views::WeaponView.new(@game),
                #MouseCollisionWrapper => lambda {|screen, enemy| puts "NOOP, could add a highlight?" },
                #TODO ugly, should this be here? not sure about design
-               VectorFollower => lambda {|screen, vf|
-                 d = 10
-                 cp = @game.camera.screen_coordinates_for(vf.current_position)
-
-                 draw_rectangle(screen, Primitives::Rectangle.new(cp, cp.plus([d,0]), cp.plus([d,d]), cp.plus([0,d])))
-                 lsi = vf.to_collision
-
-                 ls1 = @game.camera.screen_coordinates_for(lsi.p1)
-                 ls2 = @game.camera.screen_coordinates_for(lsi.p2)
-                 ls = Primitives::LineSegment.new(ls1,ls2)
-                 draw_line_segment(screen, ls)
-               }
+               VectorFollower => Views::VectorFollowerView.new(@game)
     }
     
   end
