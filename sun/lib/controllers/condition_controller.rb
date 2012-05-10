f3d7ace1af216c86bdaa9f32e618652c6fecf05d
Ben Goodspeed <b.goodspeed@gmail.com>
2012-05-10 09:42:43 -0300

    added at most

--- sun/lib/controllers/condition_controller.rb
+++ sun/lib/controllers/condition_controller.rb
@@ -10,7 +10,8 @@ class ConditionController
     @conditions = {
       "COND" => lambda {|game, arg| true}, #TODO this is pretty much equivalent to COND == NEVER rename
       "number_of_frames" => lambda {|game, frames| @game.clock.frames_rendered >= frames.to_i},
-      "player_health_at_least" => lambda {|game, hp| @game.player.health >= hp.to_i}, 
+      "player_health_at_least" => lambda {|game, hp| @game.player.health >= hp.to_i},
+      "player_health_at_most" => lambda {|game, hp| @game.player.health <= hp.to_i},
       "enemies_killed_at_least" => lambda {|game, dead| @game.player.enemies_killed >= dead.to_i }, 
       "player_near" => lambda {|game, point| @game.player.position.distance_from(point) <= @nearness },
     }
