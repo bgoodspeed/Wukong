2e56c4d27cb61d79ed04c0607b94dc7950891cff
Ben Goodspeed <b.goodspeed@gmail.com>
2012-05-25 10:27:53 -0300

    spiking some ansi c with glib

---
+++ sun/native/angv/angv/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include <glib.h>
+
+#define DEBUG
+
+#ifndef DEBUG
+    #define ASSERT(x)
+#else
+    #define ASSERT(x) \
+        if (! (x)) \
+            { \
+                printf("Failed assert on line %d in file %s: (%s)\n", __LINE__, __FILE__, #x); \
+            }
+#endif
+
+
+int main()
+{
+    printf("Hello world!\n");
+
+    GList *empty_list = NULL;
+
+    ASSERT(g_list_length(empty_list) == 0);
+
+    return 0;
+}
---
+++ sun/native/angv/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG
+
+#ifndef DEBUG
+    #define ASSERT(x)
+#else
+    #define ASSERT(x) \
+        if (! (x)) \
+            { \
+                printf("ERROR!! Assert %s failed.  %s:%d\n", #x,  __FILE__, __LINE__ ); \
+            }
+#endif
+
+
+int main()
+{
+
+    printf("Hello world!\n");
+    return 0;
+}
