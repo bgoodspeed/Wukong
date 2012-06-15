a6d4670b799947132a80e3527828b5ead88e51f0
Ben Goodspeed <b.goodspeed@gmail.com>
2012-06-15 16:17:02 -0300

    starting to wire in physics via chipmunk

--- sun/features/physics.feature
+++ sun/features/physics.feature
@@ -3,7 +3,30 @@ Feature: Physics Integration
   As a programmer
   I want to be able to inspect the physical space
 
-  Scenario: Get Static bodies from Space
-    Given I load the level "trivial"
-    When I ask for the static bodies
-    Then I should have 4 bodies
+  Scenario: Physics Wrapper
+    Given I load the game on level "physics" with screen size 640, 480
+    Then the game property "level.name" should be "'physics'"
+    Then the game property "level.physical?" should be "true"
+    Then the game property "level.physics.space.gravity.x" should be "5"
+    Then the game property "level.physics.space.gravity.y" should be "0"
+    Then the game property "level.physics.space.damping" should be "0.8"
+    Then the game property "level.physics.drop_line_location" should be "120"
+    Then the game property "level.physics.steps" should be "9"
+    Then the game property "level.physics.space.nil?" should be "false"
+    Then the game property "level.physics.space.damping" should be "0.8"
+    Then the game property "level.physics.top_wall_body.nil?" should be "false"
+    Then the game property "level.physics.bases.size" should be "2"
+    Then the game property "level.physics.enemy_base.nil?" should be "false"
+    Then the game property "level.physics.player_base.nil?" should be "false"
+    Then the game property "level.physics.bullets.size" should be "0"
+    Then the game property "level.physics.enemy_base.shape.body.p.x" should be "390"
+    Then the game property "level.physics.enemy_base.shape.body.p.y" should be "430"
+    Then the game property "level.physics.enemy_base.shape.body.m" should be "10.0"
+    Then the game property "level.physics.enemy_base.shape.body.i" should be "250.0"
+    Then the game property "level.physics.player_base.shape.body.p.x" should be "120.0"
+    Then the game property "level.physics.player_base.shape.body.p.y" should be "430"
+    Then the game property "level.physics.player_base.shape.body.m" should be "11.0"
+    Then the game property "level.physics.player_base.shape.body.i" should be "251.0"
+
+
+
--- sun/lib/models/level.rb
+++ sun/lib/models/level.rb
@@ -19,6 +19,108 @@ class LevelAnimation
     @animation_position
   end
 end
+
+class PhysicalCore
+
+end
+
+class EnemyBase
+  attr_reader :shape, :health
+  def initialize(game, shape)
+    @game = game
+    @shape = shape
+    @health = 200
+  end
+
+  def dead?
+    @health <= 0
+  end
+
+  def take_damage(m)
+    @health -= m
+    puts "enemy base health now #{@health}"
+    # bullet.body.v, bullet.body.m
+  end
+
+end
+
+class PlayerBase < EnemyBase
+
+end
+class PhysicalLevel
+  include YamlHelper
+  ATTRIBUTES = [:gravity, :damping, :steps, :drop_line_location]
+  ATTRIBUTES.each {|attr| attr_accessor(attr)}
+  attr_reader :space, :top_wall_body, :enemy_base, :player_base, :bases, :bullets
+  def initialize(game, conf)
+    @game = game
+    process_attributes(ATTRIBUTES, self, conf, {:gravity => Finalizers::GVectorFinalizer.new})
+    @space = Physics::Space.new
+    @space.gravity = Physics::Vec2.new(@gravity.x, @gravity.y) if @gravity
+    @space.damping = @damping if @damping
+    @bullets = []
+    @bases = []
+    make_wall(Physics::Vec2.new(@game.screen.width ,0), Physics::Vec2.new(@game.screen.width , @game.screen.height ))
+    make_wall(Physics::Vec2.new(0,0), Physics::Vec2.new(0, @game.screen.height ))
+    @top_wall_body = make_wall(Physics::Vec2.new(0,0), Physics::Vec2.new(@game.screen.width , 0))
+    make_wall(Physics::Vec2.new(0,@game.screen.height), Physics::Vec2.new(@game.screen.width , @game.screen.height ))
+
+    add_player_base_conf(conf["player_base"])
+    add_enemy_base_conf(conf["enemy_base"])
+  end
+
+  def build_base(mass, moment, px, py, minx, miny, maxx, maxy)
+    body = Physics::Body.new(mass, moment)
+    body.p = Physics::Vec2.new(px, py)
+    shape_array = [Physics::Vec2.new(minx, miny), Physics::Vec2.new(minx, maxy), Physics::Vec2.new(maxx, maxy), Physics::Vec2.new(maxx,miny)]
+    shape = Physics::Shape::Poly.new(body, shape_array, Physics::Vec2.new(0,0))
+    shape.collision_type = :base
+    @space.add_body(body)
+    @space.add_shape(shape)
+    shape
+  end
+
+  def add_enemy_base(mass, moment, px, py, minx, miny, maxx, maxy)
+    shape = build_base(mass, moment, px, py, minx, miny, maxx, maxy)
+    e = EnemyBase.new(@game, shape)
+    @enemy_base = e
+    shape.object = e
+    @bases << e
+  end
+
+  def add_player_base_conf(conf)
+    add_player_base(conf["mass"], conf["moment"], conf["px"], conf["py"], conf["minx"], conf["miny"],conf["maxx"],conf["maxy"],)
+  end
+  def add_enemy_base_conf(conf)
+    add_enemy_base(conf["mass"], conf["moment"], conf["px"], conf["py"], conf["minx"], conf["miny"],conf["maxx"],conf["maxy"],)
+  end
+  def add_player_base(mass, moment, px, py, minx, miny, maxx, maxy)
+    shape = build_base(mass, moment, px, py, minx, miny, maxx, maxy)
+    e = PlayerBase.new(@game, shape)
+    @player_base = e
+    shape.object = e
+    @bases << e
+  end
+
+  def physical_core_from(conf)
+    if conf["static"]
+      body = Physics::Body.new_static
+    else
+      body = Physics::Body.new(conf["mass"], conf["moment"])
+    end
+
+  end
+
+  def make_wall(p1, p2)
+    seg_body = Physics::Body.new_static
+    seg_body.p = p1
+    seg = Physics::Shape::Segment.new(seg_body, Physics::Vec2.new(0,0), p2 - p1, 1.0)
+    seg.collision_type = :wall
+    @space.add_shape(seg)
+    seg_body
+  end  
+end
+
 # Copyright 2012 Ben Goodspeed
 class Level
   ARRAY_ATTRIBUTES = [:enemies, :measurements, :line_segments, :triangles,
@@ -58,7 +160,7 @@ class Level
   end
   include YamlHelper
   include ValidationHelper
-  attr_reader :required_attributes
+  attr_reader :required_attributes, :physics
   def initialize(game=nil, in_conf={})
     @game = game
     init_arrays(ARRAY_ATTRIBUTES, self)
@@ -67,12 +169,17 @@ class Level
     @declared_enemies = {}
     @static_hash = SpatialHash.new(@cell_size)
     @dynamic_hash = SpatialHash.new(@cell_size)
+    @physics = PhysicalLevel.new(@game, conf['physics']) if conf['physics']
     @spawned_enemies = 0
     @current_hack_puzzle = ""
     @music = @game.sound_controller.add_song(@background_music, @background_music) if @background_music
     @required_attributes = YAML_ATTRIBUTES - [:player_start_position, :player_start_health,
                                               :background_music, :background_image, :reward_level, :hack_puzzle_key]
 
+  end
+
+  def physical?
+    !@physics.nil?
   end
 
   def all_backgrounds
